e0e89a20ba82dbd2d91317cf56b9fb75ffd7e437
Matthew Saul <matthewsaul22@yahoo.com>
2014-07-17 01:43:36 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	$('.main-div').append('<div class="main-div2"></div>');
+	$('.main-div2').append('<h1>Home</h1>');
+	$('.main-div2').append('<div class="pbackground"</div>'); 
+	$('.main-div2').append('<footer>Matthew Saul | Creative Design | San Francisco</footer>');
+	$('.pbackground').append('<img src="DSCN0238(reducedx2).jpg" alt="Self Portrait" height="300" width="400" class="portrait">');
+	$('.pbackground').append("<p class='fadeIn'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>");
+	$('.fadeIn').delay(2000).fadeIn(1000);
+	$('.portrait').delay(2000).fadeIn(1000);
+});
+
+$(".testing").click(function(){
+	$('.main-div2').replaceWith('<div class="main-div2"></div>');
+	$('.main-div2').append('<h1>Home</h1>');
+	$('.main-div2').append('<div class="pbackground"</div>'); 
+	$('.main-div2').append('<footer>Matthew Saul | Creative Design | San Francisco</footer>');
+	$('.pbackground').append('<img src="DSCN0238(reducedx2).jpg" alt="Self Portrait" height="300" width="400" class="portrait">');
+	$('.pbackground').append("<p class='fadeIn'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>");
+	$('.fadeIn').delay(2000).fadeIn(1000);
+	$('.portrait').delay(2000).fadeIn(1000);
+});
+
+$(".testing2").click(function(){
+	$('.main-div2').replaceWith('<div class="main-div2"></div>');
+	$('.main-div2').append('<h1>Contact</h1>');
+	$('.main-div2').append('<div class="pbackground"</div>'); 
+	$('.main-div2').append('<footer>Matthew Saul | Creative Design | San Francisco</footer>');
+	$('.pbackground').append("<p class='fadeIn'>Phone Number: (801) 647-6829</p>");
+	$('.fadeIn').delay(2000).fadeIn(1000);
+});
+
+$(".testing3").click(function(){
+	$('.main-div2').replaceWith('<div class="main-div2"></div>');
+	$('.main-div2').append('<h1>Portfolio</h1>');
+	$('.main-div2').append('<div class="pbackground"</div>'); 
+	$('.main-div2').append('<footer>Matthew Saul | Creative Design | San Francisco</footer>');
+	$('.pbackground').append("<p class='fadeIn-portfolio'>A Simple Drawing Application</p>");
+	$('.pbackground').append('<a href="simple_drawing_app/index.html"><img src="simple-drawing-app.png" alt="Simple Drawing App" height="350" width="550" class="images"></a>');
+	$('.pbackground').append("<p class='fadeIn-portfolio'>A Lightbox Example</p>");
+	$('.pbackground').append('<a href="lightbox/index.html"><img src="Lightbox.png" alt="Lightbox App" height="350" width="550" class="images"></a>');
+	$('.fadeIn-portfolio').delay(2000).fadeIn(1000);
+	$('.images').delay(2000).fadeIn(1000);
+});
+
+$(".testing4").click(function(){
+	$('.main-div2').replaceWith('<div class="main-div2"></div>');
+	$('.main-div2').append('<h1>About</h1>');
+	$('.main-div2').append('<div class="pbackground"</div>');
+	$('.main-div2').append('<footer>Matthew Saul | Creative Design | San Francisco</footer>'); 
+	$('.pbackground').append('<img src="DSCN0238(reducedx2).jpg" alt="Self Portrait" height="300" width="400" class="portrait">');
+	$('.pbackground').append("<p class='fadeIn'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>");
+	$('.fadeIn').delay(2000).fadeIn(1000);
+	$('.portrait').delay(2000).fadeIn(1000);
+});
+
